70b72a23d6eeeaa15e5327f76612ef71002c3c8d
HB <bans.harsh@gmail.com>
2025-06-14 18:44:41 -0400

    Add files via upload

---
+++ components/StepIntro.vue
@@ -0,0 +1,8 @@
+
+<template>
+  <div>
+    <h2 class="text-xl font-semibold mb-4">Welcome to PillDirect</h2>
+    <p class="mb-4">This online form will guide you through the consultation process for starting or renewing your contraceptive pill.</p>
+    <button @click="$emit('next')" class="btn">Start</button>
+  </div>
+</template>
