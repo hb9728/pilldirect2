13a814755969a5b096ef3441a2804a72d2aeb847
HB <bans.harsh@gmail.com>
2025-06-14 20:49:59 -0400

    Add files via upload

---
+++ src/components/StepScreening.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div>
+    <h2 class="text-xl font-semibold mb-4">Screening Questions</h2>
+    <input v-model="formData.firstName" placeholder="First name" class="input" />
+    <input v-model="formData.lastName" placeholder="Last name" class="input" />
+    <select v-model="formData.sex" class="input">
+      <option disabled value="">Select sex</option>
+      <option>Female</option>
+      <option>Male</option>
+      <option>Other</option>
+    </select>
+    <input v-model="formData.dob" type="date" class="input" @change="calculateAge" />
+
+    <p class="mb-3">Age: <strong>{{ calculatedAge }}</strong></p>
+    <p v-if="calculatedAge < 16" class="text-red-600 mt-2">You must be 16 or older to use this service.</p>
+
+    <div class="mt-6 flex justify-between">
+      <button @click="$emit('back')" class="btn-secondary">Back</button>
+      <button :disabled="calculatedAge < 16" @click="$emit('next')" class="btn">Next</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps(['formData'])
+
+const calculatedAge = computed(() => {
+  if (!props.formData.dob) return ''
+  const dob = new Date(props.formData.dob)
+  const today = new Date()
+  let age = today.getFullYear() - dob.getFullYear()
+  const m = today.getMonth() - dob.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+    age--
+  }
+  return age
+})
+
+function calculateAge() {
+  props.formData.age = calculatedAge.value
+}
+</script>
