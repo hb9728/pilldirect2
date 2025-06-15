56055fc364e428b0e219e9b1fa3f24d2e630321c
HB <bans.harsh@gmail.com>
2025-06-15 06:57:54 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://fhsusnyaxrmmfypmwbir.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoc3VzbnlheHJtbWZ5cG13YmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5Mzg4NTYsImV4cCI6MjA2NTUxNDg1Nn0.h_IrnWwnPQn_fy_7nqsx8Ap4_ZaUHqXn5aaCw8dgICo'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
