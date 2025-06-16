04394263c0a883b5774e5548e2dc530090073b3e
HB <bans.harsh@gmail.com>
2025-06-16 19:03:06 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import AdminLogin from '../views/AdminLogin.vue'
 
 const routes = [
   { path: '/admin/login', name: 'AdminLogin', component: AdminLogin },
-  // You can add more routes later like /admin/dashboard
 ]
 
 const router = createRouter({
