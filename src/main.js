8cc5a51844ec76190cd5fa34dec305e9d6261dee
HB <bans.harsh@gmail.com>
2025-06-16 18:33:11 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import App from './App.vue'
 import './style.css'
 import router from './router'
 
-createApp(App).mount('#app'),
-createApp(App).use(router).mount('#app')
+// createApp(App).mount('#app'),
+// createApp(App).use(router).mount('#app')
+
+createApp(App)
+  .use(router)
+  .mount('#app')
