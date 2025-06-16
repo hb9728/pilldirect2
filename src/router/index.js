fc00598688c927ec00b5c7276464ae8fb749b72c
HB <bans.harsh@gmail.com>
2025-06-16 21:53:55 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AdminLogin from '../views/AdminLogin.vue'
+import AdminDashboard from '../views/AdminDashboard.vue'
 import FormView from '../views/FormView.vue'
 
 const routes = [
   { path: '/', component: FormView },
-  { path: '/admin/login', component: AdminLogin }
+  { path: '/admin/login', component: AdminLogin },
+  { path: '/admin/dashboard', component: AdminDashboard }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
