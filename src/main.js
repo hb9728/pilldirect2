aaa2f7381ae4eae495282a949ef1c471df6bbf20
HB <bans.harsh@gmail.com>
2025-06-16 17:50:24 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import App from './App.vue'
 import './style.css'
 import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app'),
 createApp(App).use(router).mount('#app')
