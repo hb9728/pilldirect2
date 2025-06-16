1cb68e66f8f8aa60196ccd4ef1e0d5014dca0508
HB <bans.harsh@gmail.com>
2025-06-16 19:10:32 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AdminLogin from '../views/AdminLogin.vue'
 
 const routes = [
-  { path: '/admin/login', name: 'AdminLogin', component: AdminLogin },
+  { path: '/admin/login', name: 'AdminLogin', component: AdminLogin }
 ]
 
 const router = createRouter({
