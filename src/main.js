7872352af5ead6c7c75f1e1f86bb8535342be7fb
HB <bans.harsh@gmail.com>
2025-06-16 21:15:39 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router'     // our router setup
 import './style.css'
 
 const app = createApp(App)
-createApp(App).use(router).mount('#app')
+app.use(router)                  // <-- add router
+app.mount('#app')
