0dc938e129e3432d50827d1acf09a0f79cdc7a42
HB <bans.harsh@gmail.com>
2025-06-17 00:54:51 +0100

    Update AdminDashboard.vue

--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -77,40 +77,39 @@
     </table>
 
     <!-- Pagination -->
-<div class="flex items-center justify-between mt-4 flex-wrap gap-2">
-  <div v-if="totalPages > 1" class="space-x-2">
-    <button
-      :disabled="currentPage === 1"
-      @click="currentPage--"
-      class="px-3 py-1 border rounded disabled:opacity-50"
-    >Prev</button>
+    <div v-if="totalPages > 1" class="flex justify-between items-center mt-4 text-sm">
+      <div class="space-x-2">
+        <button
+          @click="changePage(currentPage - 1)"
+          :disabled="currentPage === 1"
+          class="px-3 py-1 border rounded disabled:opacity-50"
+        >Prev</button>
 
-    <button
-      v-for="page in totalPages"
-      :key="page"
-      @click="currentPage = page"
-      class="px-3 py-1 border rounded"
-      :class="{ 'bg-blue-100': currentPage === page }"
-    >{{ page }}</button>
+        <button
+          v-for="page in totalPages"
+          :key="page"
+          @click="changePage(page)"
+          class="px-3 py-1 border rounded"
+          :class="{ 'bg-blue-100': currentPage === page }"
+        >{{ page }}</button>
 
-    <button
-      :disabled="currentPage === totalPages"
-      @click="currentPage++"
-      class="px-3 py-1 border rounded disabled:opacity-50"
-    >Next</button>
-  </div>
+        <button
+          @click="changePage(currentPage + 1)"
+          :disabled="currentPage === totalPages"
+          class="px-3 py-1 border rounded disabled:opacity-50"
+        >Next</button>
+      </div>
 
-  <div class="text-sm ml-auto">
-    Show
-    <select v-model="itemsPerPage" class="border rounded p-1 text-sm ml-1">
-      <option :value="10">10</option>
-      <option :value="25">25</option>
-      <option :value="50">50</option>
-    </select>
-    per page
-  </div>
-</div>
-
+      <div>
+        Show
+        <select v-model.number="itemsPerPage" class="ml-1 border rounded p-1">
+          <option :value="10">10</option>
+          <option :value="25">25</option>
+          <option :value="50">50</option>
+        </select>
+        per page
+      </div>
+    </div>
 
     <!-- Inline Submission View -->
     <div v-if="selectedSubmission" class="mt-8 bg-white p-4 rounded shadow">
@@ -229,13 +228,6 @@ watch([filteredSubmissions, itemsPerPage], () => {
     currentPage.value = maxPage
   }
 })
-
-  watch(currentPage, (newPage) => {
-  const total = Math.ceil(filteredSubmissions.value.length / itemsPerPage.value)
-  if (newPage < 1) currentPage.value = 1
-  else if (newPage > total) currentPage.value = total
-})
-
 
 const viewSubmission = (entry) => {
   selectedSubmission.value = entry
