5b8f39e05d8b4c42f16667ffd987354f4f0f464b
HB <bans.harsh@gmail.com>
2025-06-17 19:08:23 +0100

    Update PatientPMR.vue

--- src/views/PatientPMR.vue
+++ src/views/PatientPMR.vue
@@ -2,17 +2,6 @@
   <div class="p-6">
     <h2 class="text-2xl font-bold mb-6">Patient Record</h2>
 
-    
-    <!--
-    <!-- Patient Info -->
-    <div v-if="submissions.length">
-      <div class="mb-6 bg-white p-4 rounded shadow">
-        <h3 class="text-lg font-semibold mb-2">Patient Details</h3>
-        <p><strong>Name:</strong> {{ submissions[0].firstName }} {{ submissions[0].lastName }}</p>
-        <p><strong>Email:</strong> {{ submissions[0].email }}</p>
-        <p><strong>DOB:</strong> {{ submissions[0].dob }}</p>
-      </div>
--->
 
 <!-- PMR Header -->
 <div v-if="submissions.length">
