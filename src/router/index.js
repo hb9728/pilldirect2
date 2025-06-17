3f179ea5f25b8a719043f46ded3c2d472b28a77c
HB <bans.harsh@gmail.com>
2025-06-17 16:04:10 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AdminLogin from '../views/AdminLogin.vue'
 import AdminDashboard from '../views/AdminDashboard.vue'
 import FormView from '../views/FormView.vue'
+import PatientPMR from '../views/PatientPMR.vue'
 
 const routes = [
   { path: '/', component: FormView },
   { path: '/admin/login', component: AdminLogin },
-  { path: '/admin/dashboard', component: AdminDashboard }
+  { path: '/admin/dashboard', component: AdminDashboard },
+  { path: '/admin/patient/:patientId', component: PatientPMR, props: true } 
 ]
 
 const router = createRouter({
