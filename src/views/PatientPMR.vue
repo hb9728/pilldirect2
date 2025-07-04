efb2a5964cbd8374b3ff324dc0aa1ee748f8a4e4
HB <bans.harsh@gmail.com>
2025-07-04 07:33:07 +0100

    Update PatientPMR.vue

--- src/views/PatientPMR.vue
+++ src/views/PatientPMR.vue
@@ -70,7 +70,7 @@
     <button
       v-for="(sub, idx) in filteredPatients.slice(0, 10)"
       :key="idx"
-      @click="handlePatientSelect(sub.email)"
+      @click="handleDropdownSelect(sub)"
       class="w-full text-left px-4 py-2 hover:bg-blue-50 text-sm"
     >
       {{ sub.firstName }} {{ sub.lastName }} — {{ sub.email }} — {{ sub.dob }} — {{ sub.responseId }}
